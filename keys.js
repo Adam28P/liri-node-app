fe32d1dcea3ca55fd86f770a636d343da94ce3f6
Adam P <adamparalovos@hotmail.com>
2018-09-13 02:36:21 -0400

    Added Twitter keys

--- keys.js
+++ keys.js
@@ -4,3 +4,10 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.twitter = {
+  consumer_key:  process.env.CONSUMER_KEY,
+  consumer_secret:  process.env.CONSUMER_SECRET,
+  access_token_key:  process.env.ACCESS_TOKEN_KEY,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
-var keys = require('./keys.js');
 
-var spotify = new Spotify(keys.spotify);
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+
+var spotifyKeys = new Spotify(keys.spotify);
+var twitterKeys = new twitter(keys.twitter);
 
