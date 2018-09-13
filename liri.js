961e3da464e95a00a82ca0a44099dc7e7e1957d3
Adam P <adamparalovos@hotmail.com>
2018-09-13 02:38:26 -0400

    Added cases for each task

--- liri.js
+++ liri.js
@@ -9,3 +9,46 @@ var fs = require('fs');
 var spotifyKeys = new Spotify(keys.spotify);
 var twitterKeys = new twitter(keys.twitter);
 
+var nodeArgv = process.argv;
+var command = process.argv[2];
+
+var x = "";
+//attaches multiple word arguments
+for (var i = 3; i < nodeArgv.length; i++) {
+    if (i > 3 && i < nodeArgv.length) {
+        x = x + "+" + nodeArgv[i];
+    } else {
+        x = x + nodeArgv[i];
+    }
+}
+
+//switch case
+switch (command) {
+    case "my-tweets":
+        showTweets();
+        break;
+
+    case "spotify-this-song":
+        if (x) {
+            spotifySong(x);
+        } else {
+            spotifySong("Fluorescent Adolescent");
+        }
+        break;
+
+    case "movie-this":
+        if (x) {
+            omdbData(x)
+        } else {
+            omdbData("Mr. Nobody")
+        }
+        break;
+
+    case "do-what-it-says":
+        doThing();
+        break;
+
+    default:
+        console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+        break;
+}
